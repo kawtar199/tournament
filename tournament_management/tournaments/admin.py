7324814ad3fce1d6d99a13ddbce23dea099f148e
kawtar.el-alaoui <kawtar.el-alaoui@pmu.fr>
2024-04-05 13:57:23 +0200

    update db

--- tournament_management/tournaments/admin.py
+++ tournament_management/tournaments/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Tournament, Participant, Match
+from .models import Tournament, Participant, Match,TournamentResult
 
 admin.site.register(Tournament)
 admin.site.register(Participant)
 admin.site.register(Match)
+admin.site.register(TournamentResult)
